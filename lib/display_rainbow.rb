a8539419a2f382603184942c7ed6f7460d34dc41
astronomical-chain-4979 <mdb1710@gmail.com>
2019-08-05 04:43:23 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,7 +5,7 @@ colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
 def display_rainbow(rainbow)
   puts rainbow[0].capitalize
 
-  puts "#{rainbow[0][0]}: #{rainbow[0]}, #{rainbow[1][0]}: #{rainbow[1]}, #{rainbow[2][0]}: #{rainbow[2]}, #{rainbow[3][0]}: #{rainbow[3]} , #{rainbow[4][0]}: #{rainbow[4]}, #{rainbow[5][0]}: #{rainbow[5]}, #{rainbow[6][0]}: #{rainbow[6]}  "
+  puts "#{rainbow[0][0].capitalize}: #{rainbow[0]}, #{rainbow[1][0]}: #{rainbow[1]}, #{rainbow[2][0]}: #{rainbow[2]}, #{rainbow[3][0]}: #{rainbow[3]} , #{rainbow[4][0]}: #{rainbow[4]}, #{rainbow[5][0]}: #{rainbow[5]}, #{rainbow[6][0]}: #{rainbow[6]}  "
 
 end
 
