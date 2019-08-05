8e326adb499b3e3035ab3831089b0e2cd6ce7370
astronomical-chain-4979 <mdb1710@gmail.com>
2019-08-05 04:32:01 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,11 @@
 # Write your #display_rainbow method here
 
-colors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+colors = ['Red', 'Orange', 'Yellow']
 
 def display_rainbow(rainbow)
 
-  puts "#{rainbow[0][0]}: #{rainbow[0]}, #{rainbow[1][0]}: #{rainbow[1]}, #{rainbow[2][0]}: #{rainbow[2]}, #{rainbow[3][0]}: #{rainbow[3]}, #{rainbow[4][0]}: #{rainbow[4]}, #{rainbow[5][0]}: #{rainbow[5]}, #{rainbow[6][0]}: #{rainbow[6]}  "
+  puts "#{rainbow[0][0]}: #{rainbow[0]}, #{rainbow[1][0]}: #{rainbow[1]}, #{rainbow[2][0]}: #{rainbow[2]}, #{rainbow[3][0]}: #{rainbow[3]}"
 
 end
 
-display_rainbow(colors)
+puts display_rainbow(colors)
